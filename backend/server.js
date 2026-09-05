74de9eccd03f7e2b6b699598b17d42d5ff5a57d1
Kalkidan Misganaw <kalkidanmisgadu@gmail.com>
2026-09-06 00:12:22 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,3 @@
-const { execSync } = require('child_process')
-try {
-  console.log('🔄 Generating Prisma Client...')
-  execSync('npx prisma generate', { stdio: 'inherit' })
-} catch (error) {
-  console.error('❌ Failed to generate Prisma Client:', error.message)
-}
-
 
 
 const express = require('express')
